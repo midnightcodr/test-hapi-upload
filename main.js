06ccca00c98502f63840760988a9d30bb03b6cde
Rico Chen <midnightcodr@gmail.com>
2020-04-19 00:15:49 -0400

    init

---
+++ main.js
@@ -0,0 +1,45 @@
+const Hapi = require('@hapi/hapi')
+const fs = require('fs')
+
+const handleFileUpload = file => {
+  return new Promise((resolve, reject) => {
+    const { filename } = file.hapi
+    const outFile = fs.createWriteStream(`./uploads/${filename}`)
+    file
+      .pipe(outFile)
+      .on('error', err => {
+        console.log(err)
+        reject(err)
+      })
+      .on('close', () => {
+        console.log('done')
+        resolve({
+          message: 'uploaded'
+        })
+      })
+  })
+}
+;(async () => {
+  const server = Hapi.server({
+    port: 3000
+  })
+  server.route({
+    path: '/upload',
+    method: 'POST',
+    options: {
+      payload: {
+        maxBytes: 1024 * 1024 * 100,
+        // timeout: false, // important
+        parse: true,
+        output: 'stream',
+        allow: 'multipart/form-data',
+        multipart: true
+      }
+    },
+    handler (request) {
+      return handleFileUpload(request.payload.file)
+    }
+  })
+  await server.start()
+  console.log('start started at %s', server.info.uri)
+})()
